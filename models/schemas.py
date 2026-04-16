518fec0d2193f446c38c84b4a379573dde6a104d
al3xAF <200~198654803+al3xAF@users.noreply.github.com>
2026-04-16 01:02:14 -0400

    Define Apartment dataclass schema

---
+++ models/schemas.py
@@ -0,0 +1,14 @@
+from dataclasses import dataclass
+from typing import Optional
+
+@dataclass
+class Apartment:
+	name: str
+	address: str
+	lat: float
+	lng: float
+	place_id: str
+	website: Optional[str] = None
+	commute_time_mins: Optional[int] = None
+	studio_price: Optional[int] = None
+	one_bed_price: Optional[int] = None
